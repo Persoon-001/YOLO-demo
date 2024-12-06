04b31dc4dad985df0f46b13d841c936bcad94694
sk <Steven.Kerckhofs@student.uantwerpen.be>
2024-12-06 10:40:46 +0100

    Base Demo, see attribution in README.md

---
+++ README.md
@@ -0,0 +1,36 @@
+
+
+# YOLO demo
+
+
+##  see following website for explanation of this demo
+
+https://dipankarmedh1.medium.com/real-time-object-detection-with-yolo-and-webcam-enhancing-your-computer-vision-skills-861b97c78993
+
+## Install the requirements:
+
+```commandline
+    pip install -r requirements.txt
+```
+
+## Select model:
+
+To select the model you need to change the uncommented line on lines 16 tot 20.
+
+## Select camera input:
+
+To change the camera from the default (id = 0) webcam, you need to change the id on line 5 untill you find the correct id.
+This can take a bit.
+(These seem to be relatively low values, I once had 2 and an other time 4 for my external camera.)
+
+## Change resolution:
+
+This is possible, although my webcam was not capable of more than 640 by 480 at 60FPS, 
+and my USB webcam does go to 1080p, but only at about 5 FPS.
+You can uncomment lines 11 and 12 to manually experiment with higher resolutions.
+Be aware that my bottleneck seems to be the USB camera and the bitrate available over USB, your millage will vary.
+
+## Troubleshooting: checking camera compatibility
+
+To check if the camera is working, you can run CameraTest.py,
+this should just show the camera feed of the webcam on a laptop.
---
+++ main.py
@@ -0,0 +1,75 @@
+from ultralytics import YOLO
+import cv2
+import math
+# start webcam
+cap = cv2.VideoCapture(3) # if you want ot try an external camera, try other values for his 0
+cap.set(3, 640)
+cap.set(4, 480)
+
+
+# Get the maximum supported resolution, if possible with the webcam provided.
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+
+# model
+# model = YOLO("yolo-Weights/yolo11x.pt")    # Xtra large
+# model = YOLO("yolo-Weights/yolo11l.pt")    # Large
+model = YOLO("yolo-Weights/yolo11m.pt")    # Medium
+# model = YOLO("yolo-Weights/yolo11s.pt")    # Small
+# model = YOLO("yolo-Weights/yolo11n.pt")    # Nano
+
+
+# object classes
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+
+while True:
+    success, img = cap.read()
+    results = model(img, stream=True)
+
+    # coordinates
+    for r in results:
+        boxes = r.boxes
+
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+            # put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+            print("Confidence --->",confidence)
+
+            # class name
+            cls = int(box.cls[0])
+            print("Class name -->", classNames[cls])
+
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+
+            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
